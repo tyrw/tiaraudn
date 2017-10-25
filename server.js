ec0b0c5ef28307ef37127e681b4e81e1933f4629
Tyler <tyler@eeosk.com>
2017-10-25 09:45:58 -0700

    Add robots txt

--- server.js
+++ server.js
@@ -14,6 +14,7 @@ app.get('/', (req, res) => res.sendFile('index.html', { root: publicDir }))
 app.get('/efg.js', (req, res) => res.sendFile('efg.js', { root: publicDir }))
 app.get('/data/:letter', (req, res) => res.json({ letter: req.params.letter }))
 app.get('/sitemap.xml', (req, res) => res.sendFile('sitemap.xml', { root: publicDir }))
+app.get('/robots.txt', (req, res) => res.sendFile('robots.txt', { root: publicDir }))
 app.get('/status', (req, res) => res.send('ok'))
 app.get('*', (req, res) => res.status(404).send('404'))
 
